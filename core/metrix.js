3aa063efb3a732d5a7142e006fe5798e1426e437
Brian Dixon <bjdixon@gmail.com>
2016-10-21 03:36:31 +0800

    implement metrix program

--- core/metrix.js
+++ core/metrix.js
@@ -1,58 +1,98 @@
 /**
  * metrix.js
  * provides some metrics for js submission
- * usage: metrix.js <solution> <function-name> <data>
+ * usage: metrix.js <solution>
  *
- * @path-to-solution:  The filename for your js solution
- * @function-name:     The name of the action function in solution
- * @path-to-data:      The filename containing large sample
- *                     input data to be tested against your 
- *                     provided solution
+ * @solution:  The full path to the playtime challenge
  *
- * Example: node metrix isPalindrome.js  isPalindrome  data.txt
+ * Example: node metrix /home/username/dev/playtime/fizz_buzz/
  * 
- * NOTE: In this example, data.txt may contain several palindromes
- *       separated by a new line as follows:
- *
- *       abba
- *       racecar
- *       notapalindrome
- *       superlongsuperlongsuperlongsuperlongsuperlong
- *       ... etc
- *
- *       In other cases where there are multiple input arguments
- *       each line will contain an argument followed by a 
- *       comma as follows:
- *
- *       arg1,arg2,arg3
- *       arg1,arg2,arg3
- *       ...etc 
  */
 
-(function () {
 
-  var fs = require('fs'),
-      solution = process.argv[0],
-      functionName = process.argv[1],
-      data = process.argv[2];
+const challengeDir = process.argv[2]
+const fs = require('fs')
+const path = require('path')
+const parser = require('./parser')
+const reporter = require('./reporter')
+const benchRunner = require('./benchRunner')
 
-  if (!solution || !data) {
-    console.log("Error: missing arguments");
-  } else {
-    fs.readFile(solution, {encoding: 'utf-8', flag: 'rs'}, function (err,data) {
-      if (err) {
-        return console.log(err);
-      }
-      //TODO Add code to do the following, in that order:
-      // 1 - store char count of the solution
-      // 2 - read data file
-      // 3 - take timestamp
-      // 4 - EVAL solution
-      // 5 - for each new-line in data file EVAL solution
-      //     with the provided arguments
-      // 6 - take timestamp
-      // 7 - get value of last timestamp - first timestamp
-    });
+const getDirectories = srcPath => {
+  return fs.readdirSync(srcPath).filter(file => fs.statSync(path.join(srcPath, file)).isDirectory())
+}
+
+const getFiles = srcPath => {
+  return fs.readdirSync(srcPath).filter(file => fs.statSync(path.join(srcPath, file)).isFile())
+}
+
+const parseSolutions = challengeDir => {
+  const solutions = getDirectories(challengeDir)
+  return results = solutions.map((solutionDir) => {
+    const files = getFiles(`${challengeDir}/${solutionDir}/`).filter(file => /.js$/.test(file))
+    if (files.length !== 1) {
+      return { error: `expected 1 .js file in ${solutionDir} but found ${files.length}` }
+    }
+    return Object.assign({ name: solutionDir }, parser(fs.readFileSync(path.join(challengeDir, solutionDir, files[0]), 'utf8')))
+  })
+}
+
+const benchmarkSolutions = challengeDir => {
+  const files = getFiles(challengeDir).filter(file => /.bench.js$/.test(file))
+  if (files.length !== 1) {
+    return { error: `expected 1 .bench.js file in ${challengeDir} but found ${files.length}` }
   }
+  const benchmarkFile = challengeDir.concat(files[0])
+  return {
+    benchmarkFile,
+    results: benchRunner(benchmarkFile)
+  }
+}
 
-}());
+let shouldRun = true
+let errors = []
+
+if (!challengeDir) {
+  errors.push({ error: 'missing required argument' })
+  shouldRun = false
+}
+
+if (!fs.statSync(challengeDir).isDirectory()) {
+  errors.push({ error: `${challengeDir} is not a directory` })
+  shouldRun = false
+}
+
+if (shouldRun) {
+  reporter([{ heading: `Running metrix against ${challengeDir}` }])
+  reporter([{ info: 'Analyzing solutions' }])
+  const parsedSolutions = parseSolutions(challengeDir)
+  if (parseSolutions.error != null) {
+    reporter([parseSolutions])
+  } else {
+    const results = parsedSolutions.filter(result => result.error == null).map(result => {
+      return { table: {
+        heading: ['Solution', 'Lines', 'Chars', 'Conditionals', 'Loops'],
+        data: [ result.name, result.loc, result.chars, result.conditionals, result.loops] } }
+    })
+    const errors = parsedSolutions.filter(result => result.error != null)
+    reporter(results.concat(errors))
+  }
+  reporter([{ info: 'Benchmarking solutions' }])
+  const benchmarkedSolutions = benchmarkSolutions(challengeDir)
+  if (benchmarkedSolutions.error != null) {
+    reporter([benchmarkedSolutions])
+  } else {
+    reporter([{ info: `Benchmark file: ${benchmarkedSolutions.benchmarkFile}` }])
+    benchmarkedSolutions.results.then(result => result).then(results => {
+      reporter(results.map(result => {
+        return { table: {
+            heading: ['Solution', 'Hz'],
+            data: [result.name, result.hz]
+          }
+        }
+      }))
+    }).catch(err => err)
+  }
+} else {
+  reporter(errors)
+}
+
